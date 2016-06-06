0606add97f2e4b1183851467c1fb764d85bd9cf4
vavelsky <smock.vavelsky@gmail.com>
2016-06-06 10:23:24 +0000

    status filtration + some bug changes

--- app/home/tab.controller.js
+++ app/home/tab.controller.js
@@ -2,20 +2,38 @@ export default class TabController{
     constructor($scope,$stateParams){
         this.toDos= [];
         this.user = $stateParams.user;
-        this.userText ='';
     }
     
     addToDo(){
         this.toDos.push({
-            user:this.user,
-            title:this.userText,
-            completed:false
+            value:{
+                user:this.user,
+                title:this.userText,
+                completed:false
+            },
+            editing:false
         });
+    }
+    
+    onChangeStatus(status){
+        this.statusFilter = (status === 'active') ?
+  				{ value:{completed: false }} : (status === 'completed') ?
+  				{ value:{completed: true }} : {};
+    }
+    
+    editToDo(toDo){
+        toDo.editing=!toDo.editing;
+        this.originalToDo=toDo;
+    }
+    
+    stopEditing(toDo){
+        toDo.editing=false;
+        console.log(toDo.editing);
     }
     
     removeToDo(toDo){
         var index = this.toDos.indexOf(toDo);
- +      this.toDos.splice(index,1);
+        this.toDos.splice(index,1);
     }
     
     onUser(){
