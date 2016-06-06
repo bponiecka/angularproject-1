814a15ecdfe8736224b8adbe11b533f4aadddbb5
vavelsky <smock.vavelsky@gmail.com>
2016-06-06 09:33:55 +0000

    adding removing toDos tabs

--- app/home/tab.controller.js
+++ app/home/tab.controller.js
@@ -1,6 +1,28 @@
 export default class TabController{
-    constructor($stateParams)
-    {
-     this.user = $stateParams.user;   
+    constructor($scope,$stateParams){
+        this.toDos= [];
+        this.user = $stateParams.user;
+        this.userText ='';
+    }
+    
+    addToDo(){
+        this.toDos.push({
+            user:this.user,
+            title:this.userText,
+            completed:false
+        });
+    }
+    
+    removeToDo(toDo){
+        var index = this.toDos.indexOf(toDo);
+ +      this.toDos.splice(index,1);
+    }
+    
+    onUser(){
+        this.selectedTab = 'user';
+    }
+    
+    onAll(){
+        this.selectedTab = 'all';
     }
 }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import angular from 'angular';
+import uirouter from 'angular-ui-router';
+import routing from './config';
 import home from './home';
 
 const ngModule = angular.module('app',[
